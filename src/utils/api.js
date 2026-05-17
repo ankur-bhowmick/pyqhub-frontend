ffab2300cbfb1ac696af68e4fdd92c306b847db9
ankur-bhowmick <ankurbhowmick736@gmail.com>
2026-05-17 22:46:02 +0530

    change in api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api'
+    baseURL: 'https://pyqhub-backend-k94y.onrender.com/api'
 });
 
 // Add token and handle FormData content-type
